31f651ecbc16bd3ff63f9cb9a83ea39eda236b78
Dimitri Batchevitch <dimitri.batchevitch@gmail.com>
2019-01-23 10:19:02 +0100

    réponses au questionnaire

---
+++ back/last/src/main/java/com/checkpoint/last/LastApplication.java
@@ -0,0 +1,14 @@
+package com.checkpoint.last;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastApplication.class, args);
+	}
+
+}
+
